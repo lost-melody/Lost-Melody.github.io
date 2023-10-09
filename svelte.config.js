2c8aec7856857eb98bc18afb509054258a6d11e5
lost-melody <lost__melody@163.com>
2023-10-09 10:30:00 +0800

    update svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/kit/vite";
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -15,6 +17,9 @@ const config = {
       pages: "docs",
       assets: "docs",
     }),
+    paths: {
+      base: dev ? '' : process.env.BASE_PATH,
+    },
   },
 };
 
