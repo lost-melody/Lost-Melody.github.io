3328b6e720d26edbdf03e0992b9f835fca8cdf19
lost-melody <lost__melody@163.com>
2023-10-10 01:24:46 +0800

    WIP: hamster-tools: buttonInsets editing

---
+++ src/lib/Hamster/ButtonInsets.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+    import type { ButtonInsets } from "./Hamster";
+
+    export var buttonInsets: ButtonInsets;
+    $: insets = buttonInsets.insets;
+
+    const [min, max, step] = [0x0, 0xf, 0x1];
+</script>
+
+<!-- 内距值輸入框 -->
+<div class="grid grid-cols-3 grid-rows-3 gap-1">
+    <!-- 數值内距 -->
+    <input
+        type="number"
+        bind:value={buttonInsets.value}
+        disabled={buttonInsets.expr}
+        class="input w-16 px-2 py-1 row-start-2 col-start-2"
+        placeholder="0"
+        {min}
+        {max}
+        {step}
+    />
+
+    <!-- 表達式内距: 左下上右 -->
+    <input
+        type="number"
+        bind:value={insets.left}
+        disabled={!buttonInsets.expr}
+        class="input w-16 px-2 py-1 row-start-2 col-start-1"
+        placeholder="L"
+        {min}
+        {max}
+        {step}
+    />
+    <input
+        type="number"
+        bind:value={insets.bottom}
+        disabled={!buttonInsets.expr}
+        class="input w-16 px-2 py-1 row-start-3 col-start-2"
+        placeholder="B"
+        {min}
+        {max}
+        {step}
+    />
+    <input
+        type="number"
+        bind:value={insets.top}
+        disabled={!buttonInsets.expr}
+        class="input w-16 px-2 py-1 row-start-1 col-start-2"
+        placeholder="T"
+        {min}
+        {max}
+        {step}
+    />
+    <input
+        type="number"
+        bind:value={insets.right}
+        disabled={!buttonInsets.expr}
+        class="input w-16 px-2 py-1 row-start-2 col-start-3"
+        placeholder="R"
+        {min}
+        {max}
+        {step}
+    />
+</div>
--- src/lib/Hamster/Keyboard.svelte
+++ src/lib/Hamster/Keyboard.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
     import { createEventDispatcher } from "svelte";
     import Icon from "@iconify/svelte";
-    import type { Keyboard } from "$lib/Hamster/Hamster";
+    import type { Keyboard } from "./Hamster";
+    import BtnInsetsEdit from "./ButtonInsets.svelte";
 
     // 點擊「删除」按鈕時, 發送删除事件
     const dispatch = createEventDispatcher();
@@ -13,29 +14,41 @@
     export var keyboard: Keyboard;
 </script>
 
-<div class="flex p-4 gap-4 justify-center items-center">
-    <!-- 删除按鈕 -->
-    <button class="btn-icon variant-ghost" on:click={onDelete}>
-        <Icon color="red" icon="mdi:delete" />
-    </button>
-    <!-- 鍵盤名稱編輯框 -->
-    <label class="label">
-        <span>鍵盤名</span>
-        <input
-            type="text"
-            placeholder="鍵盤名"
-            bind:value={keyboard.name}
-            class="input rounded px-2 py-1"
-        />
-    </label>
+<div class="flex flex-col justify-center items-center">
+    <!-- 鍵盤基本控件 -->
+    <div class="flex p-2 gap-4 justify-center items-center">
+        <!-- 删除按鈕 -->
+        <button class="btn-icon variant-ghost" on:click={onDelete}>
+            <Icon color="red" icon="mdi:delete" />
+        </button>
+
+        <!-- 鍵盤名稱編輯框 -->
+        <div class="flex flex-col w-32">
+            <span class="text-sm">鍵盤名</span>
+            <input
+                type="text"
+                placeholder="天行鍵"
+                bind:value={keyboard.name}
+                class="input px-2 py-1"
+            />
+        </div>
+
+        <!-- 内距「數值·表達式」切換開關 -->
+        <div class="flex flex-col">
+            <span class="text-sm">内距類型</span>
+            <button
+                on:click={() =>
+                    (keyboard.buttonInsets.expr = !keyboard.buttonInsets.expr)}
+                class="btn variant-ghost"
+            >
+                {keyboard.buttonInsets.expr ? "表達式" : "數值"}
+            </button>
+        </div>
+    </div>
+
     <!-- 鍵盤内距編輯框 -->
-    <label class="label">
-        <span>内距</span>
-        <input
-            type="text"
-            placeholder="left(2),right(2),top(2),bottom(2)"
-            value="2"
-            class="input rounded px-2 py-1"
-        />
-    </label>
+    <div class="card p-2 flex flex-col justify-center items-center">
+        <span class="text-sm">内距</span>
+        <BtnInsetsEdit bind:buttonInsets={keyboard.buttonInsets} />
+    </div>
 </div>
