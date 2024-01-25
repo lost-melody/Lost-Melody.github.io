b4e78744239621b1159e3cf82ee397a4f7db8ab4
lost-melody <lost__melody@163.com>
2024-01-25 20:20:31 +0800

    wafel: remove wafel, show into only

--- src/routes/wafel/+page.svelte
+++ src/routes/wafel/+page.svelte
@@ -1,33 +1,13 @@
 <script lang="ts">
     import Icon from "@iconify/svelte";
     import Keyboard from "$lib/Wafel/Keyboard.svelte";
-    import wafel_mappings from "$lib/assets/wafel/wafel_map.txt?raw";
     import into_mappings from "$lib/assets/wafel/into_map.txt?raw";
-
-    const mappingsList: [string, string][] = [
-        ["半音托版·Into", into_mappings],
-        ["純亂序版·Wafel", wafel_mappings],
-    ];
-    var selected: number = 0;
 </script>
 
 <div class="flex flex-col gap-2 p-2">
     <span class="p-1 m-auto rounded-md variant-soft"> 一張字根圖 </span>
-    <div class="btn-group m-auto variant-ghost">
-        {#each mappingsList as [name, _], index}
-            <button
-                disabled={selected === index}
-                on:click={() => {
-                    selected = index;
-                }}
-                class:variant-soft={selected === index}
-            >
-                {name}
-            </button>
-        {/each}
-    </div>
     <div class="flex overflow-auto">
-        <Keyboard mappings={mappingsList[selected][1]} />
+        <Keyboard mappings={into_mappings} />
     </div>
 
     <span class="p-1 m-auto rounded-md variant-soft"> 一個下載頁 </span>
