eb7ed1bfb433bfb99796200701590d8117f1882a
lost-melody <lost__melody@163.com>
2024-09-02 10:03:22 +0800

    update dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from "@sveltejs/adapter-static";
-import { vitePreprocess } from "@sveltejs/kit/vite";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 const dev = process.argv.includes("dev");
 const basePath = process.env.BASE_PATH;
@@ -8,11 +8,11 @@ const basePath = process.env.BASE_PATH;
 const config = {
     // Consult https://kit.svelte.dev/docs/integrations#preprocessors
     // for more information about preprocessors
-    preprocess: [vitePreprocess({})],
+    preprocess: vitePreprocess(),
 
     kit: {
         // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-        // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+        // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
         // See https://kit.svelte.dev/docs/adapters for more information about adapters.
         adapter: adapter({
             pages: "docs",
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,17 +6,21 @@ import { skeleton } from "@skeletonlabs/tw-plugin";
 
 const config = {
     // 2. Opt for dark mode to be handled via the class method
-    darkMode: "class",
+    darkMode: "selector",
+
     content: [
         "./src/**/*.{html,js,svelte,ts}",
         // 3. Append the path to the Skeleton package
         join(require.resolve("@skeletonlabs/skeleton"), "../**/*.{html,js,svelte,ts}"),
     ],
+
     theme: {
         extend: {},
     },
+
     plugins: [
         // 4. Append the Skeleton plugin (after other plugins)
+        require("@tailwindcss/typography"),
         skeleton({
             themes: {
                 preset: [
